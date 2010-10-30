838bc82f8f651a29f80ef1bf16538d842d9b661d
James Smith <james@floppy.org.uk>
2010-10-30 16:31:06 +0100

    remove unit validation

--- server/app/models/feed.rb
+++ server/app/models/feed.rb
@@ -1,7 +1,7 @@
 class Feed < ActiveRecord::Base
 
   validates_numericality_of :current_value, :current_carbon
-  validates_presence_of :unit, :energy_type
+  validates_presence_of :energy_type
 
   belongs_to :user
 
