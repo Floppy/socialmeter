c1ac17c6afa341396452a167a3982fa6aaaf0ecf
James Smith <james@floppy.org.uk>
2010-10-31 00:50:02 +0100

    memoize User functions

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,23 +1,28 @@
 class User < ActiveRecord::Base
 
+  extend ActiveSupport::Memoizable
+
   validates_presence_of :name
 
   has_many :feeds
 
   def friends
-    User.find(:all)
+    User.find(:all).sort{|a,b| a.current_value <=> b.current_value}
   end
 
   def current_carbon
     feeds.inject(0.0){|total, x| total += x.current_carbon}
   end
+  memoize :current_carbon
 
   def current_value
     feeds.inject(0.0){|total, x| total += x.current_value}
   end
+  memoize :current_value
 
   def human_name
     name.titleize
   end
+  memoize :human_name
 
 end
