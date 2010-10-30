0ed476d60fdf9dd6b2f3213ff5ae5b39c9ca13f9
James Smith <james@floppy.org.uk>
2010-10-30 13:57:49 +0100

    added server rails skeleton

---
+++ server/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '4c12c318780e876d5e2c48750be3bceeeace2dff3ec99c15344204f84354183cdd8cd4e34c1243d077d739c65b21c02b2c553c679147b19fce997ccd66bc9388';
