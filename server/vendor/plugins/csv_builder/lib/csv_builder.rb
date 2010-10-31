9e1cd8ca42840597849fb5882a06e8b7968b241f
James Smith <james@floppy.org.uk>
2010-10-31 11:41:20 +0000

    fix bug with missing CSV options

--- server/vendor/plugins/csv_builder/lib/csv_builder.rb
+++ server/vendor/plugins/csv_builder/lib/csv_builder.rb
@@ -40,7 +40,7 @@ module ActionView # :nodoc:
             controller.response.headers['Content-Disposition'] = "attachment; filename=\#{@filename}"
           end
 
-          FasterCSV.generate do |faster_csv|
+          FasterCSV.generate(@csv_options) do |faster_csv|
             csv = TransliteratingFilter.new(faster_csv)
             #{template.source}
           end
