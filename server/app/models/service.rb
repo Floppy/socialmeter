5779db5f6ea93748c0330cd50a40c40a26683d1c
James Smith <james@floppy.org.uk>
2010-10-31 01:49:18 +0000

    Add twitter update skeleton

--- server/app/models/service.rb
+++ server/app/models/service.rb
@@ -6,6 +6,9 @@ class Service < ActiveRecord::Base
   validates_uniqueness_of :name, :scope => :user_id
   validates_uniqueness_of :external_id, :scope => :name
 
+  before_save :update_avatar
+  after_save :update_friends
+
   def self.service_list
     SERVICE_LIST
   end
@@ -13,5 +16,21 @@ class Service < ActiveRecord::Base
   belongs_to :user
   has_many :service_friends
   has_many :friends, :through => :service_friends, :source => :user
+
+  def update_friends
+    send "update_#{name.downcase}_friends"
+  end
+
+  def update_avatar
+    send "update_#{name.downcase}_friends"
+  end
+
+  def update_twitter_friends
+
+  end
+
+  def update_twitter_avatar
+
+  end
 
 end
