b56ed27ff83fd22214fe2e983aa446aae8b620f7
James Smith <james@floppy.org.uk>
2012-05-10 23:06:43 +0100

    don't need AMEE profile

--- server/app/models/feed.rb
+++ server/app/models/feed.rb
@@ -1,6 +1,6 @@
 class Feed < ActiveRecord::Base
 
-  has_amee_profile
+  #has_amee_profile
 
   validates_numericality_of :current_value, :current_carbon, :average_value, :average_carbon
   validates_presence_of :energy_type
