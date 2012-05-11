3f8fcf5b84fa6422c2d3e68f30163473112ab265
James Smith <james@floppy.org.uk>
2012-05-11 22:39:36 +0100

    Upgrade rails framework

--- server/config/initializers/secret_token.rb
+++ server/config/initializers/secret_token.rb
@@ -2,6 +2,6 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
+# Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '4c12c318780e876d5e2c48750be3bceeeace2dff3ec99c15344204f84354183cdd8cd4e34c1243d077d739c65b21c02b2c553c679147b19fce997ccd66bc9388';
+SocialMeter::Application.config.secret_token = '3205ecdce3503daea55d7af88cd647f91fa56090ff259f7811718bb6c0edb86e23bd35a6af72a564dfbab468a7b47f17db9ec8d930c408fd732334b3b3a45c38'
--- server/config/initializers/session_store.rb
+++ server/config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_server_session',
-  :secret      => '065ebf35cec86f92dc005d0cc268f3d9b8892a147526473762aa0c1796c02ef866448e4fd6820c0a4e96322d34a09990166d57887540843365be6a7f2ff748da'
-}
+SocialMeter::Application.config.session_store :cookie_store, key: '_server_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# (create the session table with "rails generate session_migration")
+# SocialMeter::Application.config.session_store :active_record_store
