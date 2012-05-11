7a5e1db779c47bf7ea29026ade55ad24cf520d2f
James Smith <james@floppy.org.uk>
2012-05-11 23:03:16 +0100

    None of these tests are any use
    Not surprising really, it was a hackday.

--- server/spec/models/service_friend_spec.rb
+++ server/spec/models/service_friend_spec.rb
@@ -1,14 +1,5 @@
 require 'spec_helper'
 
 describe ServiceFriend do
-  before(:each) do
-    @valid_attributes = {
-      :service_id => 1,
-      :user_id => 1
-    }
-  end
 
-  it "should create a new instance given valid attributes" do
-    ServiceFriend.create!(@valid_attributes)
-  end
 end
--- server/spec/models/service_spec.rb
+++ server/spec/models/service_spec.rb
@@ -1,14 +1,5 @@
 require 'spec_helper'
 
 describe Service do
-  before(:each) do
-    @valid_attributes = {
-      :service_type => "value for service_type",
-      :external_id => "value for external_id"
-    }
-  end
 
-  it "should create a new instance given valid attributes" do
-    Service.create!(@valid_attributes)
-  end
 end
