476cab8542891f95cc65b663329658756238a535
James Smith <james@floppy.org.uk>
2012-05-12 01:04:07 +0100

    Automatically set up cosm triggers

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+
 class Feed < ActiveRecord::Base
 
   #has_amee_profile
@@ -8,5 +10,26 @@ class Feed < ActiveRecord::Base
   belongs_to :user
 
   attr_accessible :external_id, :energy_type, :current_value, :current_carbon
+
+  after_create :create_cosm_trigger
+  
+  def create_cosm_trigger
+    # Prepare post data
+    env, stream = external_id.split(':')
+    post_data = {
+      :url =>  "http://socialmeter.floppy.org.uk/users/#{user.id}/feeds/#{id}/cosm_trigger",
+      :trigger_type => "change",
+      :environment_id => env,
+      :stream_id => stream
+    }
+    # Do post
+    uri = URI('http://api.cosm.com/v2/triggers/')
+    Net::HTTP.start(uri.host, uri.port) do |http|
+      request = Net::HTTP::Post.new uri.request_uri
+      request['X-ApiKey'] = COSM_API_KEY
+      request.body = post_data.to_json
+      http.request request
+    end
+  end
 
 end
